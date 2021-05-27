92a6f8df7d327deb8cacd057cee8858ef0949018
Paul Kukiel <kukiel@amazon.com>
2021-05-27 16:50:53 +1000

    Some clean up and error handling

--- pdf-maker/app.js
+++ pdf-maker/app.js
@@ -12,27 +12,48 @@ exports.generate_pdf = async (event, context) => {
     margin: { top: ".5in", right: ".5in", bottom: ".5in", left: ".5in" },
   };
 
-  const pdf = await getPDF(html, options);
+  const pdf = await getPDF(html, options)
+  if(pdf.success){
+    return {
+      headers: {
+        "Content-type": "application/pdf",
+      },
+      statusCode: 200,
+      body: pdf.data.toString("base64"),
+      isBase64Encoded: true,
+    }
 
-  return {
-    headers: {
-      "Content-type": "application/pdf",
-    },
-    statusCode: 200,
-    body: pdf.toString("base64"),
-    isBase64Encoded: true,
-  };
+  }else{
+    return {
+      headers: {
+        "Content-type": "application/json",
+      },
+      statusCode: 500,
+      body: pdf.error,
+      isBase64Encoded: false,
+    }
+  }
+  
 }
 
 /// https://github.com/alixaxel/chrome-aws-lambda
 getPDF = async (html, options) => {
-  const executablePath =  await chromium.executablePath
-  const browser  = await chromium.puppeteer.launch({ args: chromium.args, executablePath })
-  const page = await browser.newPage()
-  const loaded = page.waitForNavigation({ waitUntil: "load" })
-  await page.setContent(html)
-  await loaded
-  const result = await page.pdf(options)
-  await browser.close()
+  let result, browser
+  try {
+    const executablePath =  await chromium.executablePath
+    browser  = await chromium.puppeteer.launch({ args: chromium.args, executablePath })
+    let page = await browser.newPage()
+    const loaded = page.waitForNavigation({ waitUntil: "load" })
+    await page.setContent(html)
+    await loaded
+    const pdf = await page.pdf(options)
+    result = { data: pdf, success: true, error: null }
+  } catch (error) {
+      result = { data: null, success: false, error: error }
+  } finally {
+    if (browser !== null) {
+      await browser.close();
+    }
+  }
   return result
 }
